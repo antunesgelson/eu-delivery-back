f0f50da1d070e9bd74aa6c0c2f8610d190bac8e2
Gelson <gelsondeveloper@hotmail.com>
2024-09-09 16:11:34 -0300

    🌟feat<buscarUsuario>: -Implementa GET para buscar informações do usuário.

--- src/modulos/usuario/usuario.controller.ts
+++ src/modulos/usuario/usuario.controller.ts
@@ -1,17 +1,21 @@
-import { Body, Controller, Put } from "@nestjs/common";
-import { IsPublic } from "../auth/decorators/isPublic.decorator";
+import { Body, Controller, Get, Put, Req } from "@nestjs/common";
 import { UsuarioEditarDTO } from "./dto/usuarioEditar.dto";
 import { UsuarioEditarService } from "./services/UsuarioEditar.service";
+import { BuscarUsuarioService } from "./services/buscarUsuario.service";
 
 @Controller('usuario')
-export class UsuarioController{
-
-    constructor(private usuarioEditarService:UsuarioEditarService){
-
-    }
+export class UsuarioController {
+    constructor(private usuarioEditarService: UsuarioEditarService,
+        private usuarioBuscarService: BuscarUsuarioService
+    ) { }
 
     @Put()
-    async editar(@Body() usuarioEditarDTO:UsuarioEditarDTO){
+    async editar(@Body() usuarioEditarDTO: UsuarioEditarDTO) {
         return this.usuarioEditarService.exec(usuarioEditarDTO);
+    }
+
+    @Get()
+    async buscar(@Req() request) {
+        return this.usuarioBuscarService.exec({ usuarioId: request.user.sub });
     }
 }
--- src/modulos/usuario/usuario.module.ts
+++ src/modulos/usuario/usuario.module.ts
@@ -1,17 +1,18 @@
 import { forwardRef, Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthModules } from "../auth/auth.modules";
-import { AdicionarUsuarioService } from "./services/adicionarUsuario.service";
-import { UsuarioEntiy } from "./usuarios.entity";
-import { UsuarioController } from "./usuario.controller";
 import { UsuarioEditarService } from "./services/UsuarioEditar.service";
+import { AdicionarUsuarioService } from "./services/adicionarUsuario.service";
+import { BuscarUsuarioService } from "./services/buscarUsuario.service";
+import { UsuarioController } from "./usuario.controller";
+import { UsuarioEntiy } from "./usuarios.entity";
 
 
 @Module({
     imports: [TypeOrmModule.forFeature([UsuarioEntiy]),
     forwardRef(() => AuthModules),],
     controllers: [UsuarioController],
-    providers: [AdicionarUsuarioService,UsuarioEditarService],
+    providers: [AdicionarUsuarioService, UsuarioEditarService, BuscarUsuarioService],
     exports: [AdicionarUsuarioService]
 })
 export class UsuarioModule { }
