6de4cc363e2b5d6b37257577584caebb3cf5dffd
thiago <thiago.1310@live.com>
2024-10-03 16:25:00 -0300

    pode adicionar tanto 1 item quanto um array

--- src/modulos/produto/produto.controller.ts
+++ src/modulos/produto/produto.controller.ts
@@ -1,9 +1,9 @@
-import {  Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Query, Req,  SetMetadata,  UploadedFile,  UploadedFiles, UseInterceptors } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Query, Req, SetMetadata, UploadedFile, UploadedFiles, UseInterceptors } from "@nestjs/common";
 import { AddProductIngrentDTO } from "./dto/addProductIngredient.dto";
 import { ProdutoDTO } from "./dto/produto.dto";
 import { ProdutoService } from "./produto.service";
 import { IsPublic } from "../auth/decorators/isPublic.decorator";
-import {  FileInterceptor, FilesInterceptor } from "@nestjs/platform-express";
+import { FileInterceptor, FilesInterceptor } from "@nestjs/platform-express";
 import { DeletarFotoProdutoDTO } from "./dto/deletarFotoProduto.dto";
 import { DeletarProdutoDTO } from "./dto/deletarProduto.dto";
 import { AddFotoProdutoDTO } from "./dto/AddFotoProduto.dto";
@@ -15,39 +15,51 @@ import { DeletarAdicionalProdutoDTO } from "./dto/deletarAdicionalProduto.dto";
 export class ProdutoController {
     constructor(
         private produtoService: ProdutoService,
-    ) {}
+    ) { }
 
     @Post('cadastrar')
-    @SetMetadata('isAdmin',true)
-    @UseInterceptors(FilesInterceptor('imgs',10,{limits:{fileSize:5*1024*1024}}))  // Lida com o campo de arquivo 'img'
+    @SetMetadata('isAdmin', true)
+    @UseInterceptors(FilesInterceptor('imgs', 10, { limits: { fileSize: 5 * 1024 * 1024 } }))  // Lida com o campo de arquivo 'img'
     async cadastrarProduto(
         @Body() produtoDto: ProdutoDTO,
         @UploadedFiles() files: Express.Multer.File[],
     ) {
-        const produtos = {...produtoDto,files:files}
+        const produtos = { ...produtoDto, files: files }
         return this.produtoService.adicionar(produtos);
     }
 
     @Post('adicional')
-    @SetMetadata('isAdmin',true)
-    async adicionarAdicional(@Body() addAdicionarProdutoDTO:AddAdicionalProdutoDTO){
-        return this.produtoService.adicionarAdicional(addAdicionarProdutoDTO);
+    @SetMetadata('isAdmin', true)
+    async adicionarAdicional(@Body() produto: AddAdicionalProdutoDTO | AddAdicionalProdutoDTO[]) {
+        if(Array.isArray(produto)){
+            return Promise.all(produto.map(p=> this.produtoService.adicionarAdicional(p)));
+        }else{ return this.produtoService.adicionarAdicional(produto);}
     }
 
+    @Post('adicionar-ingredientes')
+    @SetMetadata('isAdmin', true)
+    async addProductIngredient(@Body() produto: AddProductIngrentDTO | AddProductIngrentDTO[]) {
+        if (Array.isArray(produto)) {
+            return Promise.all(produto.map(p => this.produtoService.adicionarIngredientes(p)));
+        } else { return this.produtoService.adicionarIngredientes(produto);}
+    }
+
+
     @Delete('adicional')
-    @SetMetadata('isAdmin',true)
-    async deletarAdicional(@Body() produtoDTO:DeletarAdicionalProdutoDTO){
+    @SetMetadata('isAdmin', true)
+    async deletarAdicional(@Body() produtoDTO: DeletarAdicionalProdutoDTO) {
         return this.produtoService.deletarAdicional(produtoDTO)
     }
 
+
     @Post('/foto/:produtoId')
-    @SetMetadata('isAdmin',true)
-    @UseInterceptors(FileInterceptor('img',{limits:{fileSize:5*1024*1024}}))  // Lida com o campo de arquivo 'img'
+    @SetMetadata('isAdmin', true)
+    @UseInterceptors(FileInterceptor('img', { limits: { fileSize: 5 * 1024 * 1024 } }))  // Lida com o campo de arquivo 'img'
     async cadastrarFotoProduto(
         @Param() fotoDTO: AddFotoProdutoDTO,
         @UploadedFile() file: Express.Multer.File,
     ) {
-        const foto = {...fotoDTO,file:file}
+        const foto = { ...fotoDTO, file: file }
         return this.produtoService.adicionarFoto(foto);
     }
 
@@ -63,21 +75,17 @@ export class ProdutoController {
         return this.produtoService.buscarPorId(id);
     }
 
-    @Post('adicionar-ingredientes')
-    @SetMetadata('isAdmin',true)
-    async addProductIngredient(@Body() produto: AddProductIngrentDTO) {
-        return this.produtoService.adicionarIngredientes(produto);
-    }
+
 
     @Delete(':produtoId')
-    @SetMetadata('isAdmin',true)
-    async deletarProduto(@Param() produtoDTO:DeletarProdutoDTO){
+    @SetMetadata('isAdmin', true)
+    async deletarProduto(@Param() produtoDTO: DeletarProdutoDTO) {
         return this.produtoService.deletarProduto(produtoDTO);
     }
 
     @Delete('/:produtoId/:etag')
-    @SetMetadata('isAdmin',true)
-    async deletarFotoProduto(@Param() produtoDTO:DeletarFotoProdutoDTO){
+    @SetMetadata('isAdmin', true)
+    async deletarFotoProduto(@Param() produtoDTO: DeletarFotoProdutoDTO) {
         return this.produtoService.deletarFotoProduto(produtoDTO);
     }
 
